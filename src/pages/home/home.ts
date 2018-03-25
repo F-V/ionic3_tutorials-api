3f14f1c8c7ad68bc4253d5f72edcf2908cbd1209
F-V <32668262+F-V@users.noreply.github.com>
2018-03-25 23:25:58 +0200

    using the simple-php-api
    this uses the https://github.com/F-V/simple-php-api

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,47 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ApiServiceProvider } from '../../providers/api-service/api-service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  users: ArrayBuffer;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private apiService: ApiServiceProvider) {
 
   }
 
+  ionViewDidLoad(){
+    //this.createDemoUser();
+    this.readUsers();
+    //this.updateDemoUser();
+    //this.deleteDemoUser();
+    
+  }
+  createDemoUser(){
+    this.apiService.post("users",{username:"test",email:"test@outlock.com",password:"anotherhash"}).subscribe(result=>{
+      console.log("got this as a response of creating a new user",result);
+    });
+  }
+
+  readUsers(){
+    this.apiService.get("users").subscribe((result)=>{
+      console.log("got this from  api",result);
+      this.users =  result;
+    });
+  }
+
+  updateDemoUser() {
+    this.apiService.put("users/2",{username:"updateduser"}).subscribe(result=>{
+      console.log("got this as a response while updating the user",result);
+    });
+  }
+
+  deleteDemoUser(){
+    this.apiService.delete("users/2").subscribe(result=>{
+      console.log("got this as a response while deleting the user",result);
+    })
+  }
 }
---
+++ src/providers/api-service/api-service.ts
@@ -0,0 +1,56 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the ApiServiceProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ApiServiceProvider {
+
+  url: string = 'http://localhost:8080/extern/api.php';
+
+  constructor(public http: HttpClient) {
+
+  }
+
+  get(endpoint: string, params?: any, reqOpts?: any) {
+    if (!reqOpts) {
+      reqOpts = {
+        params: new HttpParams()
+      };
+    }
+
+    // Support easy query params for GET requests
+    if (params) {
+      reqOpts.params = new HttpParams();
+      for (let k in params) {
+        reqOpts.params.set(k, params[k]);
+      }
+    }
+
+    return this.http.get(this.url + '/' + endpoint, reqOpts);
+  }
+
+  post(endpoint: string, body: any, reqOpts?: any) {
+    if(reqOpts){
+      reqOpts.headers = reqOpts.headers.set('Content-Type','application/x-www-form-urlencoded');
+    }
+    
+    return this.http.post(this.url + '/' + endpoint, body, reqOpts);
+  }
+
+  put(endpoint: string, body: any, reqOpts?: any) {
+    return this.http.put(this.url + '/' + endpoint, body, reqOpts);
+  }
+
+  delete(endpoint: string, reqOpts?: any) {
+    return this.http.delete(this.url + '/' + endpoint, reqOpts);
+  }
+
+  patch(endpoint: string, body: any, reqOpts?: any) {
+    return this.http.put(this.url + '/' + endpoint, body, reqOpts);
+  }
+}
